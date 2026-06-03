1d765c4e53a538dc78577bb547392927546fa9e6
Lucario <155163046+Lucaclarkz@users.noreply.github.com>
2026-06-03 10:14:16 +0630

    Add files via upload

---
+++ flix_copy_esp32C3.ino
@@ -0,0 +1,48 @@
+// Copyright (c) 2023 Oleg Kalachev <okalachev@gmail.com>
+// Repository: https://github.com/okalachev/flix
+
+// Main firmware file
+
+#include "vector.h"
+#include "quaternion.h"
+#include "util.h"
+
+extern float t, dt;
+extern float controlRoll, controlPitch, controlYaw, controlThrottle, controlMode;
+extern Vector gyro, acc;
+extern Vector rates;
+extern Quaternion attitude;
+extern bool landed;
+extern float motors[4];
+extern void setLED(bool value);
+extern void updateLED();
+
+void setup() {
+	Serial.begin(115200);
+	print("Initializing flix\n");
+	setupParameters();
+	setupPower();
+	setupLED();
+	setLED(true);
+	setupMotors();
+	setupWiFi();
+	setupIMU();
+	setupRC();
+	setLED(false);
+	print("Initializing complete\n");
+}
+
+void loop() {
+	readIMU();
+	step();
+	readRC();
+	estimate();
+	updateLED();
+	control();
+	sendMotors();
+	handleInput();
+	processMavlink();
+	readVoltage();
+	logData();
+	syncParameters();
+}
